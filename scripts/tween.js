1c7242fa76145c376f2d1a510f417dbb87838eb6
Mark McArthey <mcarthey@gmail.com>
2016-02-07 18:57:50 -0600

    Tweaked tween sample
    Added check for tween completion and added code to prevent multiple
    clicks until tween complete.  Also added collision detection with
    createjs.hittest()

--- scripts/tween.js
+++ scripts/tween.js
@@ -1,12 +1,11 @@
-  var circle;
-  var pos = 0;
+var circle;
+var pos = 0;
 
-  function init() {
-    
-    var radius = 80;
-    var stage = new createjs.Stage("demoCanvas");
+function init() {
+	radius = 80;
+	stage = new createjs.Stage("demoCanvas");
 
-    circle = new createjs.Shape();
+	circle = new createjs.Shape();
 
 	circle.graphics.setStrokeStyle(3, "round", "round")
 		.beginFill("plum")
@@ -17,27 +16,43 @@
 		.moveTo(0,0)
 		.lineTo(radius,0);
 
-    circle.x = 100;
-    circle.y = 100;
+	circle.x = 100;
+	circle.y = 100;
 
-    circle.addEventListener("click", onclick);
+	circle.addEventListener("click", onclick);
 
-    stage.addChild(circle);
+	circle.shadow = new createjs.Shadow("#000000", 4, 7, 10);
 
-    // createjs.Tween.get(circle, { loop: true })
-    //   .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-    //   .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-    //   .to({ alpha: 0, y: 225 }, 100)
-    //   .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-    //   .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+	stage.addChild(circle);
 
-      createjs.Ticker.setFPS(60);
-      createjs.Ticker.addEventListener("tick", stage);
-  }
+	// createjs.Tween.get(circle, { loop: true })
+	//   .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+	//   .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+	//   .to({ alpha: 0, y: 225 }, 100)
+	//   .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+	//   .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+
+	createjs.Ticker.setFPS(30);
+	createjs.Ticker.addEventListener("tick", tick);
+}
 
 function onclick() {
-
+	circle.removeEventListener("click", onclick);
 	pos += 90;
 	createjs.Tween.get(circle)
-      .to({ rotation: pos }, 1000, createjs.Ease.backInOut);
+      .to({ rotation: pos }, 1000, createjs.Ease.backInOut)
+      .call(tweenComplete);
+}
+
+function tweenComplete() {
+	console.log('rotation complete');
+	circle.addEventListener("click", onclick);
+}
+
+function tick(event) {
+	circle.alpha = 0.2;
+	if (circle.hitTest(stage.mouseX, stage.mouseY)) { 
+		circle.alpha = 1; 
+	}
+	stage.update(event);
 }
