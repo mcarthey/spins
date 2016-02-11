c9df5f148e765579dc0cd1b80a45a828c448a44f
Mark McArthey <mcarthey@gmail.com>
2016-02-11 12:39:53 -0600

    Finalized static example with HTML debug output
    Created HTML table with images and debug output to console to identify a
    'touched' display object and whether it is connected to the items around
    it.

--- scripts/grid.js
+++ scripts/grid.js
@@ -8,11 +8,15 @@ var clock1 = 3; // 0011
 var clock2 = 6; // 0110
 var clock3 = 9;  // 1001
 var clock4 = 12; // 1100
+var GRIDX = 2;
+var GRIDY = 2;
+var arr;
 
 function init()
 {
-	var arr = createMatrix(2,2);
+	arr = createMatrix(GRIDX,GRIDY);
 
+	// Create
 	for (i=0;i<arr.length;i++) {
 		for (j=0;j<arr[i].length;j++) {
 			s = new ShapeObject();
@@ -20,9 +24,17 @@ function init()
 			s.color = randomColor();
 			arr[i][j] = s;
 	
-			checkConnection(arr[i][j]);
 		}
 	}
+		
+	// Simulate check
+	for (i=0;i<arr.length;i++) {
+		for (j=0;j<arr[i].length;j++) {
+			checkConnection(i,j); // pass in coordinates
+		}
+	}
+
+	document.write(makeTableHTML(arr));
 }
 
 function createMatrix(length) {
@@ -52,34 +64,79 @@ function randomRange(max) {
 	return Math.floor(Math.random()*max);
 }
 
-function isConnected() {
-	var connected = (clock2 * 4 % 15 & clock1 & direction) != 0;
-	return connected;
-}
-
-function checkConnection(s) {
+function checkConnection(x,y) {
 	var left = 1;
 	var below = 2;
 	var right = 4;
 	var above = 8;
-	console.log(pad(dec2bin(s.clock), 4));
-	
+
+	var newX, newY;
+
+	s = arr[x][y];
+
+	c1 = pad(dec2bin(s.clock), 4);
+	console.log('touched clock:'+c1+ ' at ' + x + ','+ y);
+
 	bit = s.clock & left;
 	if (bit > 0) {
-		console.log('left');
+		console.log('checking left'); // -x
+		newX = x - 1;
+		if (newX >= 0) {
+			c2 = arr[newX][y].clock;
+			connected = isConnected(s.clock, c2, left);
+			console.log(c1 +' at ' + x + ',' + y + ' isConnected to ' + pad(dec2bin(c2), 4) + ' at ' + newX + ',' + y +'?:'+connected);
+		}
+		else {
+			console.log('isConnected?:N/A');
+		}
 	}
+
 	bit = s.clock & below;
 	if (bit > 0) {
-		console.log('below');
+		console.log('checking below'); // +y
+		newY = y + 1;
+		if (newY < GRIDY) {
+			c2 = arr[x][newY].clock;
+			connected = isConnected(s.clock, c2, below);
+			console.log(c1 +' at ' + x + ',' + y + ' isConnected to ' + pad(dec2bin(c2), 4) + ' at ' + x + ',' + newY +'?:'+connected);
+		}
+		else {
+			console.log('isConnected?:N/A');
+		}		
 	}
+
 	bit = s.clock & right;
 	if (bit > 0) {
-		console.log('right');
+		console.log('checking right'); // +x
+		newX = x + 1;
+		if (newX < GRIDX) {
+			c2 = arr[newX][y].clock;
+			connected = isConnected(s.clock, c2, right);
+			console.log(c1 +' at ' + x + ',' + y + ' isConnected to ' + pad(dec2bin(c2), 4) + ' at ' + newX + ',' + y +'?:'+connected);
+		}
+		else {
+			console.log('isConnected?:N/A');
+		}
 	}
 	bit = s.clock & above;
 	if (bit > 0) {
-		console.log('above');
+		console.log('checking above'); // -y
+		newY = y - 1;
+		if (newY >= 0) {
+			c2 = arr[x][newY].clock;
+			connected = isConnected(s.clock, c2, above);
+			console.log(c1 +' at ' + x + ',' + y + ' isConnected to ' + pad(dec2bin(c2), 4) + ' at ' + x + ',' + newY +'?:'+connected);
+		}
+		else {
+			console.log('isConnected?:N/A');
+		}		
+
 	}
+}
+
+function isConnected(clock1, clock2, direction) {
+	var connected = (clock2 * 4 % 15 & clock1 & direction) != 0;
+	return connected;
 }
 
 function dec2bin(dec){
@@ -92,3 +149,16 @@ function pad(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
+function makeTableHTML(myArray) {
+    var result = '<table border=1>';
+    for(var i=0; i<myArray.length; i++) {
+        result += '<tr>';
+        for(var j=0; j<myArray[i].length; j++){
+            result += '<td>'+'<img src="images/' + pad(dec2bin(myArray[i][j].clock),4)+ '.png"></img>'+'</td>';
+        }
+        result += '</tr>';
+    }
+    result += '</table>';
+
+    return result;
+}
